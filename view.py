11484dbf9a65f5b26cd5e3487de95c2b3f82a68d
Xuan Truong Nguyen <pa.xt.ng@gmail.com>
2017-09-15 14:00:25 +0200

    fix secret key

--- view.py
+++ view.py
@@ -23,6 +23,8 @@ class CustomFlask(Flask):
 
 
 app = CustomFlask(__name__)
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 
 @app.route("/")
@@ -121,8 +123,6 @@ def send_color(path_file):
 
 
 if __name__ == "__main__":
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
 
     app.debug = True
     app.run()
