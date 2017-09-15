4d6fb505031bf9d69fd798b8c41b55538523f089
Xuan Truong Nguyen <pa.xt.ng@gmail.com>
2017-09-15 17:27:56 +0200

    Date picker from JQUERY-UI

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -14,51 +14,56 @@
 //     // $('#dateD').datetimepicker();
 // });
 
-$(function() {
-  // $(".form_datetime").datetimepicker({
-  //   format: "dd MM yyyy"
-  // });
-
-  $('#dateA').datetimepicker(
-    {format: "dd/mm/yyyy"}
-  );
-  $('#dateD').datetimepicker();
-});
-
-
-$('#dateA').change(function() {
-  $.get('getPrice.php',
-        {arrivee: $('#dateA').prop('value'), type: $("type_room").prop('value')}, function (data) {
-          $('#prix').prop('value', data);
-        });
-});
-
-$('#dateD').change(function() {
-  $.get('getPrice.php',
-      {arrivee: $('#dateA').prop('value'), type: $("type_room").prop('value'), depart: $('#dateD').prop('value')},
-      function (data) {
-        $('#prix').prop('value', data);
-  });
-});
-
-$('#type_room').change(function() {
-  $.get('getPrice.php',
-        {arrivee: $('#dateA').prop('value'), type: $("type_room").prop('value')}, function (data) {
-          $('#prix').prop('value', data);
-        });
-});
-
-$('#btnContactUs').click(function(){
-    alert("Merci! Votre message a été bien enregistré ");
-});
+// $(function() {
+//   // $(".form_datetime").datetimepicker({
+//   //   format: "dd MM yyyy"
+//   // });
+//
+//   $('#dateA').datetimepicker(
+//     {format: "dd/mm/yyyy"}
+//   );
+//   $('#dateD').datetimepicker();
+// });
 
 
+// $('#dateA').change(function() {
+//   $.get('getPrice.php',
+//         {arrivee: $('#dateA').prop('value'), type: $("type_room").prop('value')}, function (data) {
+//           $('#prix').prop('value', data);
+//         });
+// });
 
-$.get("ReceptionMessage.php", {
-  id: "1",
-  email: "nguyenthanhmary@yahoo.fr",
-   "subject", "message"},
-       function(ReceptionMessage){
-         $("div#result").html(data);
-       });
-});
+// $('#dateD').change(function() {
+//   $.get('getPrice.php',
+//       {arrivee: $('#dateA').prop('value'), type: $("type_room").prop('value'), depart: $('#dateD').prop('value')},
+//       function (data) {
+//         $('#prix').prop('value', data);
+//   });
+// });
+
+// $('#type_room').change(function() {
+//   $.get('getPrice.php',
+//         {arrivee: $('#dateA').prop('value'), type: $("type_room").prop('value')}, function (data) {
+//           $('#prix').prop('value', data);
+//         });
+// });
+
+// $('#btnContactUs').click(function(){
+//     alert("Merci! Votre message a été bien enregistré ");
+// });
+//
+//
+
+// $.get("ReceptionMessage.php", {
+//   id: "1",
+//   email: "nguyenthanhmary@yahoo.fr",
+//    "subject", "message"},
+//        function(ReceptionMessage){
+//          $("div#result").html(data);
+//        });
+// });
+
+$( function() {
+  $( "#dateA" ).datepicker();
+  $( "#dateD" ).datepicker();
+} );
